e8215acc726b914b965b45875a34e44796e13e6b
Doreyd Mehila <dmehila@gmail.com>
2022-09-14 22:43:13 -0600

    Fixed a few Major Bugs for VM

--- pages/api/databases/adddatabase.js
+++ pages/api/databases/adddatabase.js
@@ -1,8 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export default function handler(req, res) {
-
-  const url = 'mongodb://localhost:27017';
+  const url = "mongodb://localhost:27017";
 
   MongoClient.connect(url, function (err, db) {
     const { newDatabase } = JSON.parse(req.body);
@@ -11,7 +10,7 @@ export default function handler(req, res) {
     return db
       .db(newDatabase)
       .collection("dummyCollection")
-      .insertOne({ dummyData: "dummyData" }, function (err, result) {
+      .insertOne({ key: "value" }, function (err, result) {
         if (err) throw err;
         res.status(200).json(result);
       });
